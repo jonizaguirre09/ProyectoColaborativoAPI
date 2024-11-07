3502ab6eca1a14e32e6b3fa5b1efaa4ce2ec86cf
jonizaguirre09 <102174461+jonizaguirre09@users.noreply.github.com>
2024-11-07 15:11:38 +0100

    Update app.py

--- API_2/app.py
+++ API_2/app.py
@@ -7,7 +7,7 @@ import json
 
 app = Flask(__name__)
 app.secret_key = 'mi_clave_secreta'  # Cambia esto por una clave segura
-openai.api_key = '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'  # Reemplaza por tu clave de OpenAI
+openai.api_key = ''  # Reemplaza por tu clave de OpenAI
 
 # Configuración de flask-login
 login_manager = LoginManager()
